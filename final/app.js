a375daeef0da834e1cc1c9fc47000a0bdc503a84
mitesh <ShriMiTesHcHauDHary@gmail.com>
2024-02-02 13:56:55 +0530

    2-20-24

---
+++ final/app.js
@@ -0,0 +1,29 @@
+// const btn = document.querySelectorAll(".question-btn")
+
+// btn.forEach(function(btn){
+//     btn.addEventListener("click", function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.toggle("show-text")
+//     })
+// })
+
+
+const questions = document.querySelectorAll(".question")
+
+questions.forEach(function(question){
+    let btn = question.querySelector(".question-btn")
+    // console.log(btn);
+
+    btn.addEventListener("click", function(){
+
+        questions.forEach(function (item) {
+                    if (item !== question) {
+                      item.classList.remove("show-text");
+                    }
+                  });
+       
+        
+question.classList.toggle("show-text")
+    })
+
+})
